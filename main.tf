505f9753594f3fb436cb28fcb73db22660c4c7cb
yashraj.dighe <yashraj.dighe@unthinkable.co>
2022-10-03 14:43:05 +0530

    updated ec2 workflow and terraform code

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  subnet_id     = var.subnet_id
 
   tags = {
     Name = "github-actions"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "ami_owner" {
   default = ["099720109477"] # canonical
   type    = list(any)
 }
+
+variable "subnet_id" {
+  default = "subnet-06c642911a6600923"
+  type    = string
+}
